b0873b2fed1506734d3602593f449588e8f53ac5
Yan <yanmvp1@gmail.com>
2022-08-22 14:19:29 -0300

    map

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,69 @@
-import Header from "../src/components/header";
-import Secao from "../src/components/Secao";
-import Footer from "../src/components/footer";
 import * as S from "../styles/styles";
 
-
-
 function Home() {
-    const mostrar = 1
-    const link = 1
 
-    const secao = 'componente'
+
+    const lista = [
+        {
+            Nome: "Nome: Yan",
+            idade: "Idade: 21",
+            data_de_nacimento: "Data de Nacimento: 08/01/2001",
+            nome_da_mae: "Nome da Mãe: Luciana",
+            nome_do_pai: "Nome do Pai: Erivaldo"
+        },
+        {
+            Nome: "Nome: Malu",
+            idade: "Idade: 19",
+            data_de_nacimento: "Data de Nacimento: 08/01/2003",
+            nome_da_mae: "Nome da Mãe: Elena",
+            nome_do_pai: "Nome do Pai: Marcelo"
+        },
+        {
+            Nome: "Nome: Luciana",
+            idade: "Idade: 41",
+            data_de_nacimento: "Data de Nacimento: 03/06/1980",
+            nome_da_mae: "Nome da Mãe: Concilia",
+            nome_do_pai: ""
+        },
+        {
+            Nome: "Nome: Neymar jn",
+            idade: "Idade: 30",
+            data_de_nacimento: "Data de Nacimento: 05/02/1992",
+            nome_da_mae: "Nome da Mãe: Nadine",
+            nome_do_pai: ""
+        },
+        {
+            Nome: "Nome: Cristiano Ronaldo",
+            idade: "Idade: 37",
+            data_de_nacimento: "Data de Nacimento: 05/02/1985",
+            nome_da_mae: "Nome da Mãe: Maria Dolores",
+            nome_do_pai: ""
+        }
+    ]
 
     return (
         <div className='App'>
+            {
+                lista.map(lista => {
 
+                    return (
+                        <S.Secao>
+                        <S.Lista>{lista.Nome}</S.Lista>
+                        <div>{lista.idade}</div>
+                        <div>{lista.data_de_nacimento}</div>
+                        <div>{lista.nome_da_mae}</div>
+                        <div>{lista.nome_do_pai}</div>
+                        </S.Secao>
+                    )
 
+                }
+                )
+            }
         </div>
 
     );
 }
 export default Home;
 
-//Vai fazer 4 cards que usem o mesmo componente porem mostre textos diferentes
-
-// Mostrar 2 links que consomem o mesmo componente que vai ser passado um boleno props, se um passar true exibe o link, se eu passar false ou não 
-// passar nada , não exibe o link
+//fazer uma lista exibindo5 pessoas com nome, idade, date de nacimento e nome da mãe, e para 3 pessoa
+// o nome do pai porem quem não tiver o pai nem o titulo de pai
--- styles/styles.js
+++ styles/styles.js
@@ -1,9 +1,16 @@
 import styled from 'styled-components'
 
+export const Secao = styled.div`
+    border: 1px solid ;
+    border-radius: 10px;
+    width: 250px;
+    margin-bottom: 20px;
+    margin-left: 50px;
+    padding-left: 20px;
+`
+
+export const Lista = styled.div`
 
 
-export const Link = styled.a`
-
-color: red;
 
 `
