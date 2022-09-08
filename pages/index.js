eaed212d2eddd0cb3d16a8fe7e1d8ca615b71ed7
Yan <yanmvp1@gmail.com>
2022-09-08 12:15:01 -0300

    PDP

--- pages/index.js
+++ pages/index.js
@@ -4,69 +4,18 @@ import { Teste, Click } from '../src/utils'
 import axios from 'axios';
 
 function Home() {
-    const [resposta, setResposta] = useState()
-    const [time, setTime] = useState()
-    const [numero, setNumero] = useState()
-    const [jogador, setJogador] = useState()
-    const [DadoEditado, setDadoEditado] = useState()
-    useEffect(() => {
-        axios.get("https://teste-aula-metodos-7ba07-default-rtdb.firebaseio.com/camisetas.json")
-            .then(function (response) {
-                setResposta(response.data);
-            })
-    }, [])
-    const Cadastro = () => {
-        axios.post("https://teste-aula-metodos-7ba07-default-rtdb.firebaseio.com/camisetas.json", {
-            time: time,
-            numero: numero,
-            jogador: jogador
-        })
-            .then(function (response) {
-                alert('Seu time foi Adicionado')
-            }).catch(function () {
-                alert('Não Conseguimos Cadrastrar seu Time')
-            })
-    }
-    const deletar = (id) => {
-        axios.delete(`https://teste-aula-metodos-7ba07-default-rtdb.firebaseio.com/camisetas/${id}.json`, {})
-            .then(function (response) {
-                alert('Seu Time foi Excluido')
-            }).catch(function () {
-                alert('Não Conseguimos Excluir seu Time')
-            })
-    }
+    const [Usuario, setUsuario] = useState()
+    const entrar = () => {
 
-    const editar = (id) => {
-        axios.patch(`https://teste-aula-metodos-7ba07-default-rtdb.firebaseio.com/camisetas/${id}.json`, {time:DadoEditado})
-            .then(function (response) {
-                alert('Seu Time foi Editado')
-            }).catch(function () {
-                alert('Não Conseguimos Editar seu Time')
-            })
     }
     return (
         <>
-            <h1>Times NBA</h1>
-            <>
-                {resposta && Object.entries(resposta).map((NBA) => {
-                    return (
-                        <>
-                            {NBA[1].time}
-                            <input onChange={(e)=>{setDadoEditado(e.target.value)}}/>
-                            <button onClick={() => deletar(NBA[0])}>Excluir</button>
-                            <button onClick={() => editar(NBA[0])}>Editar</button><br />
-                        </>)
-                })}
-
-            </>
-            <h2>Adicionar Time</h2>
-            <form>
-                <label>Time: </label><input onChange={(e) => setTime(e.target.value)} /><br />
-                <button onClick={(e) => {
-                    e.preventDefault()
-                    Cadastro()
-                }}>Adicionar</button>
-            </form>
+            <h1>Login</h1><br />
+            <label>Usuario</label>
+            <input onChange={(e) => setUsuario(e.target.value)} /><br />
+            <label>Senha</label>
+            <input type='password' /><br />
+            <button onClick={() => entrar()}>Entrar</button>
         </>
     );
 }
